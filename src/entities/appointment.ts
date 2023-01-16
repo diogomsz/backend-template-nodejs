ead032e92c564847c2cd0e2aebd85379f6bef70b
Diogo Martins de Assis <diogoassis3301@gmail.com>
2023-01-16 19:46:57 -0300

    create a appointment ts and spec

---
+++ src/entities/appointment.ts
@@ -0,0 +1,26 @@
+export interface IAppointmentProps {
+    customer: string;
+    startsAt: Date;
+    endsAt: Date;
+}
+    
+
+export class Appointment {
+    private props: IAppointmentProps;
+
+    get customer() {
+        return this.props.customer;
+    }
+
+    get startsAt() {
+        return this.props.startsAt;
+    }
+
+    get endsAt() {
+        return this.props.endsAt;
+    }
+
+    constructor(props: IAppointmentProps) {
+        this.props = props;
+    }
+}

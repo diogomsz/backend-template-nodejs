8e817d900b48c73f6cbb6ac6a9a59a6610fbbf5b
Diogo Martins de Assis <diogoassis3301@gmail.com>
2023-01-16 20:13:34 -0300

    some tests

--- src/entities/appointment.spec.ts
+++ src/entities/appointment.spec.ts
@@ -11,3 +11,19 @@ test('create an appointment', () => {
     expect(appointment).toBeInstanceOf(Appointment);
     expect(appointment.customer).toEqual('John Doe');
 }); 
+
+test('cannot create an appointment with end date before start date', () => {
+    const startsAt = new Date();
+    const endsAt = new Date();
+
+    // on that situation the appointment start today and finish yesterday
+    endsAt.setDate(endsAt.getDate() - 1);
+
+    expect(() => {
+        return new Appointment({
+            customer: 'John Doe',
+            startsAt,
+            endsAt
+        });
+    }).toThrow();
+});
